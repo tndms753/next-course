bb02d1bbaa57bc82461de99b5ab86fa8f83737db
tndms753 <tndms753@naver.com>
2021-07-21 21:24:59 +0900

    add next link

---
+++ pages/clients/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+function ClientsPage() {
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ];
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {/* <li>
+          <Link href="/clients/max">Maximilian</Link>
+        </li>
+        <li>
+          <Link href="/clients/manu">Manuel</Link>
+        </li> */}
+        {clients.map(client => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ClientsPage;
